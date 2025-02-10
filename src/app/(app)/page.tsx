8cb80b6880133925d53a16d1d4abb19993ff88cf
Leopold Bauer <Leopold.Bauer@titanom.com>
2025-02-10 09:43:42 +0100

    fix: error handling

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,9 +1,8 @@
 import ParticleAnimation from '@/components/particle-animation';
-import Link from 'next/link';
 
 export default function Page() {
   return (
-    <main className="h-screen flex items-center justify-center">
+    <main className="h-screen flex items-center justify-center k">
       <ParticleAnimation />
       <div className="flex flex-col justify-between w-full px-8 gap-8">
         <div>
@@ -11,14 +10,15 @@ export default function Page() {
           <h1 className="text-white text-8xl max-md:text-5xl">Bauer</h1>
         </div>
         <div>
-          <Link
+          {/* <Link
             href="https://formfast.bauerleopold.de"
             className="border-white border-2 rounded text-white px-2 py-4 hover:bg-surface"
             target="_blank"
             rel="noopener noreferrer"
           >
             Form Fast
-          </Link>
+          </Link> */}
+          <p className='text-white'>coming soon...</p>
         </div>
       </div>
     </main>
