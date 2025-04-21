54ef6cc2dde64183d855dce692893d62e530eeb0
Leopold Bauer <Leopold.Bauer@titanom.com>
2025-04-21 20:03:03 +0200

    fix: link width

--- src/app/(app)/main-content.tsx
+++ src/app/(app)/main-content.tsx
@@ -7,19 +7,20 @@ function MainContent() {
                 <h1 className="text-white text-8xl max-md:text-5xl">Leopold</h1>
                 <h1 className="text-white text-8xl max-md:text-5xl">Bauer</h1>
             </div>
-            <div>
+            <div className="flex">
                 <Link
                     href="https://pertolo.iieo.de"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    <div className="border-white border-2 rounded text-white px-6 py-4 hover:bg-surface w-fit hover:scale-105 transition-transform duration-200">
+                    <div className="border-white border-2 rounded text-white px-6 py-4 hover:bg-surface hover:scale-105 transition-transform duration-200">
                         <h2 className="text-2xl">Pertolo</h2>
                         <p className="text-sm">The Drinking Game</p>
                     </div>
                 </Link>
             </div>
-        </div>);
+        </div>
+    );
 }
 
 export default MainContent;
