360ecd22cadf2de435b9353ea2d8a246d7bfc959
Leopold Bauer <Leopold.Bauer@titanom.com>
2025-05-12 08:55:33 +0200

    feat: added verein

--- src/app/(app)/main-content.tsx
+++ src/app/(app)/main-content.tsx
@@ -8,7 +8,7 @@ function MainContent() {
                 <h1 className="text-white text-8xl max-md:text-5xl">Bauer</h1>
             </div>
             <div className="h-[50dvh] flex py-4">
-                <div className="flex">
+                <div className="flex gap-4">
                     <Link
                         href="https://pertolo.iieo.de"
                         target="_blank"
@@ -16,8 +16,19 @@ function MainContent() {
                         style={{ pointerEvents: "auto" }}
                     >
                         <div className="border-white border-2 rounded text-white px-6 py-4 hover:bg-surface hover:scale-105 transition-transform duration-200">
-                            <h2 className="text-2xl">Pertolo</h2>
+                            <h2 className="text-xl">Pertolo</h2>
                             <p className="text-sm">The Drinking Game</p>
+                        </div>
+                    </Link>
+                    <Link
+                        href="https://verein.iieo.de"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        style={{ pointerEvents: "auto" }}
+                    >
+                        <div className="border-white border-2 rounded text-white px-6 py-4 hover:bg-surface hover:scale-105 transition-transform duration-200">
+                            <h2 className="text-xl">Vereinsmanager</h2>
+                            <p className="text-sm">Verwalte deinen Verein</p>
                         </div>
                     </Link>
                 </div>
